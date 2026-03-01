126644c79556dc8fccd77bf67d01fe5102704a41
Andrey Gulyayko <guliaiko@llcglobaladvisors.com>
2026-03-01 12:25:46 +0400

    docstring color

--- assets/js/python-plus-mode.js
+++ assets/js/python-plus-mode.js
@@ -1,17 +1,17 @@
 /* global CodeMirror */
-CodeMirror.defineMode("python-plus", function(config, parserConfig) {
-    const pythonMode = CodeMirror.getMode(config, "python");
+CodeMirror.defineMode("", function(config, parserConfig) {
+    const pythonMode = CodeMirror.getMode(config, "python-plus");
 
     return CodeMirror.multiplexingMode(
         pythonMode,
         {
             open: '"""', close: '"""',
-            mode: CodeMirror.getMode(config, "python"),
+            mode: CodeMirror.getMode(config, "python-plus"),
             delimStyle: "docstring"
         },
         {
             open: "'''", close: "'''",
-            mode: CodeMirror.getMode(config, "python"),
+            mode: CodeMirror.getMode(config, "python-plus"),
             delimStyle: "docstring"
         }
     );
